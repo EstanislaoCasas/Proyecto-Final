5be0308c2be134407c2d3c3582200ec0c140e3af
EstanislaoCasas <estanicasas@gmail.com>
2020-04-22 17:19:07 -0300

    Terminé todas las views

--- routes/users.js
+++ routes/users.js
@@ -12,4 +12,12 @@ router.get('/register', function(req, res, next) {
   res.render('register', usersController);
 });
 
+router.get('/my-proyects', function(req, res, next) {
+  res.render('myProyects', usersController);
+});
+
+router.get('/edit', function(req, res, next) {
+  res.render('edit', usersController);
+});
+
 module.exports = router;
